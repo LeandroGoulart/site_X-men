66f97a5ed8245afa9d96ebbeed969ffcd22d401a
LGoulart <leandrovgoulart@gmail.com>
2023-07-20 15:09:22 -0300

    refatorar

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,37 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
-        personagem.classList.add('selecionado');
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-selecionado img');
-        const idpersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `./src/images/card-${idpersonagem}.png`;
+        if(window.innerWidth > 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
 
-        const nomePersonagem = document.querySelector('.nome-personagem');
-        nomePersonagem.innerText = personagem.getAttribute('data-name');
+        selecaoPersonagem(personagem);
 
-        const descricaoPersonagem = document.querySelector('.descricao-personagem');
-        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+        alterarImagemGrande(personagem);
+
+        alterarNomeDescricao(personagem);
 
 
     });
 });
+
+function alterarNomeDescricao(personagem) {
+    const nomePersonagem = document.querySelector('.nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+    const descricaoPersonagem = document.querySelector('.descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarImagemGrande(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-selecionado img');
+    const idpersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/images/card-${idpersonagem}.png`;
+}
+
+function selecaoPersonagem(personagem) {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+    personagem.classList.add('selecionado');
+}
+
