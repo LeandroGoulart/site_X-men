5cfdb43f62303d94fb8eb0182d2a24cf1d33684d
LGoulart <leandrovgoulart@gmail.com>
2023-07-20 14:57:28 -0300

    adaptação javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-package 
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-selecionado img');
+        const idpersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/images/card-${idpersonagem}.png`;
+
+        const nomePersonagem = document.querySelector('.nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.querySelector('.descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+
+
+    });
+});
